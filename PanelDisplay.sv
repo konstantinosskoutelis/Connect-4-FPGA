01c862173e5d52960b30ea4ba1da2b0e659b0978
konstantinosskoutelis <skouteliskonstantinos@gmail.com>
2020-06-15 19:17:17 +0300

    WORKING pushed to master branch

--- PanelDisplay.sv
+++ PanelDisplay.sv
@@ -33,30 +33,11 @@ always_ff @(posedge clk, negedge rst) begin
     h_counter <= '0;
   end else begin
     if (enable) begin
-<<<<<<< HEAD
-      case (count_horizontal)
-        799: begin
-          count_horizontal <= 0;
-          case(count_vertical)
-            526: begin
-              count_vertical <= 0;
-            end
-            default begin
-              count_vertical <= count_vertical + 1;
-            end
-          endcase
-        end
-        default begin
-          count_horizontal <= count_horizontal + 1;
-        end
-      endcase
-=======
       if (h_limit) begin
           h_counter <= '0;
       end else begin
           h_counter <= h_counter + 1'b1;
       end
->>>>>>> e157bb8e6f4187d05e95d8d7287fe003f0f1ac5a
     end
   end
 end
@@ -103,20 +84,13 @@ winner_sprite win(
   .data   (w_data)
 );
 
+
 //coloring
 localparam offset = 60;
+int v_start;
+int h_start;
 
 always_comb begin
-<<<<<<< HEAD
-
-  w_addr = ((count_vertical-20)*90)+(count_horizontal-530);
-  // Borders & background - check
-  if (count_vertical>=440 || count_horizontal>=630 || count_vertical<10 || count_horizontal<10) begin
-    rgb = 0;
-  end else if (count_horizontal>=510 && count_horizontal<520) begin
-    rgb = 0;
-  end else if (count_horizontal>=530 && count_horizontal<620 && count_vertical>=20 && count_vertical<74) begin //SCOREBOARD
-=======
   w_addr = ((v_counter-20)*90)+(h_counter-530);
   // Borders & background - check
   if (v_counter>=440 || h_counter>=630 || v_counter<10 || h_counter<10) begin
@@ -124,7 +98,6 @@ always_comb begin
   end else if (h_counter>=510 && h_counter<520) begin
     rgb = 3'b000;
   end else if (h_counter>=530 && h_counter<620 && v_counter>=20 && v_counter<74) begin //SCOREBOARD    
->>>>>>> e157bb8e6f4187d05e95d8d7287fe003f0f1ac5a
     if (w_data) begin
       case(winner)
         2'b01: begin
@@ -143,20 +116,6 @@ always_comb begin
   end else begin
     rgb = 3'b001;    
   end
-<<<<<<< HEAD
-
-  t_addr = 0;
-  for (int i=0; i<=5; i++) begin 
-    for (int j=0; j<=6; j++) begin      
-      if(v_pos[i] & h_pos[j])  begin
-        //rgb = {panel[5-i][j],1'b0};
-        t_addr = ((count_vertical-(20+(70*i)))*60)+(count_horizontal-(20+(70*j)));
-        if (~t_data) begin
-          rgb = {panel[5-i][j],1'b0};
-        end
-      end
-    end
-=======
   
   // Game panel 
   t_addr = 0;
@@ -175,9 +134,8 @@ always_comb begin
     end else begin 
      h_start = 0;
    end
->>>>>>> e157bb8e6f4187d05e95d8d7287fe003f0f1ac5a
   end
-
+  
   // Player position - check
   if(v_counter>=(450) && v_counter<(470)) begin
     for (int j=0; j<=6; j++) begin
@@ -188,27 +146,6 @@ always_comb begin
           rgb = 3'b100;
         end
       end
-    end
-  end
-end
-
-logic [5:0] v_pos;
-logic [6:0] h_pos;
-
-always_comb begin
-  for (int i=0; i<=5; i++) begin
-    if(count_vertical>=20+(70*i) && count_vertical<20+(70*i)+offset ) begin
-      v_pos[i]=1;
-    end else begin
-      v_pos[i]=0;
-    end
-  end
-
-  for (int j=0; j<=6; j++) begin
-    if(count_horizontal>=20+(70*j) && count_horizontal<20+(70*j)+offset) begin
-      h_pos[j]=1;
-    end else begin
-      h_pos[j]=0;
     end
   end
 end
